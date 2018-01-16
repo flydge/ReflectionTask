decbd69cf50f416f5e3fb26a9cf97867e157ecd2
flydge <followflydge@gmail.com>
2018-01-16 18:11:54 +0300

    Develop TypeInfo class that displays certain info about inputted class (list of methods, constructors, etc)

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+
+namespace ReflectionTask
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Type myType = typeof(string);
+            TypeInfo typeInfo = new TypeInfo();
+            Console.WriteLine(typeInfo.GetInterfaceInfo(myType));
+            Console.WriteLine(typeInfo.GetMethodsInfo(myType));
+            Console.WriteLine(typeInfo.GetConstructorsInfo(myType));
+            Console.WriteLine(typeInfo.GetFieldsInfo(myType));
+            Console.WriteLine(typeInfo.GetPropertiesInfo(myType));
+            
+        }
+    }
+}
---
+++ TypeInfo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionTask
+{
+    public class TypeInfo
+    {
+        private string infoString;
+        
+        public string GetInterfaceInfo(Type myType)
+        {
+            infoString = "\nInterfaces:\n";
+            foreach (var interfaceInfo in myType.GetInterfaces())
+            {
+                infoString += interfaceInfo.Name + "; ";
+            }
+            return infoString;
+        }
+
+        public string GetMethodsInfo(Type myType)
+        {
+            infoString = "\nPublic instance declared only methods:\n";
+            foreach (var methodInfo in myType.GetMethods(BindingFlags.Public|BindingFlags.Instance|BindingFlags.DeclaredOnly))
+            {
+                String parameterString = "";
+                ParameterInfo[] parameterInfos = methodInfo.GetParameters();
+
+                for (int i = 0; i < parameterInfos.Length; i++)
+                {
+                    parameterString += parameterInfos[i].ParameterType.Name + " " + parameterInfos[i].Name;
+                    if (parameterInfos.Length > i+1)
+                    {
+                        parameterString += " , ";
+                    }
+                }
+                
+                infoString += methodInfo.ReturnType.Name + " " +methodInfo.Name + "("+ parameterString + ");\n";
+                
+            }
+            return infoString;
+        }
+
+
+        public string GetConstructorsInfo(Type myType)
+        {
+            infoString = "\nAll constructors:\n";
+            foreach (var constructorInfo in myType.GetConstructors())
+            {
+                string constrInfoString = "";
+
+                ParameterInfo[] constructorInfos = constructorInfo.GetParameters();
+
+                for (int i = 0; i < constructorInfos.Length; i++)
+                {
+                    constrInfoString += constructorInfos[i].ParameterType.Name + " " + constructorInfos[i].Name;
+                    if (constructorInfos.Length > i+1)
+                    {
+                        constrInfoString += ";";
+                    }
+                   
+                }
+
+                infoString += myType.Name + "(" + constrInfoString + ");\n";
+
+            }
+            return infoString;
+        }
+
+
+        public string GetFieldsInfo(Type myType)
+        {
+            infoString = "\nAll fields:\n";
+            foreach (var fieldInfo in myType.GetFields())
+            {
+                infoString += fieldInfo.FieldType.Name + " " + fieldInfo.Name + ";";
+            }
+            return infoString;
+        }
+
+        public string GetPropertiesInfo(Type myType)
+        {
+            infoString = "\nInstance non public properties:\n";
+            foreach (var propInfo in myType.GetProperties(BindingFlags.Instance | BindingFlags.NonPublic))
+            {
+                infoString += propInfo.PropertyType.Name + " " + propInfo.Name + ";\n";
+            }
+            return infoString;
+        }
+        
+    }
+}
